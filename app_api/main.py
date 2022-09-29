358b25c70b82752c539b58f435cf706941cfca18
nedrayua <nedrayua@gmail.com>
2022-09-29 13:01:02 +0300

    some changes in structure of api add blueprint

--- app_api/main.py
+++ app_api/main.py
@@ -2,6 +2,10 @@ from app import app
 import view
 
 from config import WEBAPP_HOST, WEBAPP_PORT
+from api.api import api
+
+app.register_blueprint(api, url_prefix='/api')
+
 
 if __name__ == '__main__':
     app.run(host=WEBAPP_HOST, port=WEBAPP_PORT)
